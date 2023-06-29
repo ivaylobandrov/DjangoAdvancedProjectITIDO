bb3b36e6f52fbec8b96efa66ed3db1fc0901aa5e
Ivaylo Bandrov <ivaylobandrov@Ivaylos-MacBook-Pro.local>
2023-06-29 16:40:29 +0300

    removed unnecessary lines

--- app/portal/management/commands/importcsv.py
+++ app/portal/management/commands/importcsv.py
@@ -8,21 +8,16 @@ from portal.models import PricesAndQuantities, BlockProduct, HourProducts
 
 def validate_date_format(value):
     try:
-        # Extract the last 5 characters (date) from the input value
         date_str = value[-5:]
 
-        # Append the current year to the date
         current_year = datetime.now().year
         date_with_year = f"{current_year}-{date_str}"
 
-        # Convert the date to the desired format (YYYY-MM-DD)
         validated_date = datetime.strptime(date_with_year,
                                            '%Y-%m/%d').date()
 
-        # Format the date as YYYY-MM-DD
         formatted_date = validated_date.strftime('%Y-%m-%d')
 
-        # Remove the day name and comma from the input value
         value = value.replace(date_str, formatted_date).replace(
             value[:4], '')
 
@@ -35,20 +30,16 @@ def validate_date_format(value):
 
 def convert_price_to_decimal_number(number, decimal_separator=",",
                                     thousand_separator=" "):
-    print("NUMBER: ", number)
-    # Remove non-numeric characters and replace separators
+
     cleaned_number = number.replace(thousand_separator,
                                     '').replace(decimal_separator,
                                                 '.')
 
-    # Create a Decimal object with the cleaned number as a string
     decimal_number = decimal.Decimal(cleaned_number)
 
-    # Round the decimal to two decimal places
     rounded_decimal = decimal_number.quantize(
         decimal.Decimal('0.00'))
 
-    # Convert the decimal to a string with separators
     decimal_string = format(rounded_decimal, f',.2f').replace(',',
                                                               thousand_separator)
 
@@ -57,10 +48,8 @@ def convert_price_to_decimal_number(number, decimal_separator=",",
 
 def convert_volumes_to_decimal_number(value):
     try:
-        # Remove any non-numeric characters and replace comma with dot
         cleaned_value = value.replace(" ", "").replace(",", ".")
 
-        # Create a Decimal object with the cleaned value as a string
         decimal_number = decimal.Decimal(cleaned_value)
 
         return decimal_number
@@ -87,8 +76,6 @@ class Command(BaseCommand):
         hours = rows[8][3:]
         bgn_per_mwh = rows[9][3:]
         mwh = rows[10][3:]
-
-        # TODO Since we have three models the information from the csv file should be separated accordingly
 
         for i in range(len(dates)):
             formatted_data = validate_date_format(dates[i])
